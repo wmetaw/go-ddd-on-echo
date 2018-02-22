6a3dfd6a5ee4893bfd936b71cac7678b8e6f563a
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-02-22 20:06:36 +0900

    ドメインのコメントを追加

--- domain/repository/user.go
+++ domain/repository/user.go
@@ -2,7 +2,11 @@ package repository
 
 import "github.com/wmetaw/go-ddd-on-echo/domain"
 
-// infrastructureで実装を期待する
+// Domain Service層
+
+// infrastructure層の「infrastructure/persistance/user_repository」が
+// 実装するメソッド(インターフェース) を定義する
+// これにより、モデルがインフラに依存しなくなり、依存性の逆転が生まれる（インフラ側の実装を別のDBに差し替え易い）
 type UserRepository interface {
 	Get(id int) (*domain.User, error)
 	GetAll() ([]*domain.User, error)
--- domain/user.go
+++ domain/user.go
@@ -1,5 +1,11 @@
 package domain
 
+// DomainModel層(実クラス)
+// DDDではEntityと呼ばれる
+// 基本的にクラス(構造体を定義し、データと簡単な振る舞いのみを持たせる)
+
+// モデルはドメインの知識を持ち、不変条件を満たすと良い
+// (モデル設計ドメインの知識を持つということ http://little-hands.hatenablog.com/entry/2017/10/04/201201 )
 type User struct {
 	Id   uint `gorm:"primary_key"`
 	Name string
